434d7e7e1766bf06e9eb59334ac5f8020be32b21
Jason Molenda <jmolenda@apple.com>
1999-08-02 23:47:56 +0000

    import gdb-1999-08-02 snapshot

--- readline/savestring.c
+++ readline/savestring.c
@@ -1,13 +1,13 @@
 /* savestring.c  */
 
-/* Copyright (C) 1998,2003 Free Software Foundation, Inc.
+/* Copyright (C) 1998 Free Software Foundation, Inc.
 
    This file is part of the GNU Readline Library, a library for
    reading lines of text with interactive input and history editing.
 
    The GNU Readline Library is free software; you can redistribute it
    and/or modify it under the terms of the GNU General Public License
-   as published by the Free Software Foundation; either version 2, or
+   as published by the Free Software Foundation; either version 1, or
    (at your option) any later version.
 
    The GNU Readline Library is distributed in the hope that it will be
@@ -18,20 +18,16 @@
    The GNU General Public License is often shipped with GNU software, and
    is generally kept in a file called COPYING or LICENSE.  If you do not
    have a copy of the license, write to the Free Software Foundation,
-   59 Temple Place, Suite 330, Boston, MA 02111 USA. */
-#define READLINE_LIBRARY
+   675 Mass Ave, Cambridge, MA 02139, USA. */
 
-#include <config.h>
-#ifdef HAVE_STRING_H
-#  include <string.h>
-#endif
-#include "xmalloc.h"
+extern char *strcpy ();
+extern char *xmalloc ();
 
 /* Backwards compatibility, now that savestring has been removed from
    all `public' readline header files. */
 char *
 savestring (s)
-     const char *s;
+     char *s;
 {
-  return ((char *)strcpy ((char *)xmalloc (1 + strlen (s)), (s)));
+  return ((char *)strcpy (xmalloc (1 + (int)strlen (s)), (s)));
 }
