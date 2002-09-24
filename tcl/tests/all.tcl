07296cfdb73a6d68eb6b921fd25c7c9dacdf1eec
Keith Seitz <keiths@redhat.com>
2002-09-24 20:24:18 +0000

    import tk 8.4.0

--- tcl/tests/all.tcl
+++ tcl/tests/all.tcl
@@ -1,25 +1,19 @@
 # all.tcl --
 #
-# This file contains a top-level script to run all of the Tcl
-# tests.  Execute it by invoking "source all.test" when running tcltest
+# This file contains a top-level script to run all of the Tk
+# tests.  Execute it by invoking "source all.tcl" when running tktest
 # in this directory.
 #
 # Copyright (c) 1998-1999 by Scriptics Corporation.
-# Copyright (c) 2000 by Ajuba Solutions
 #
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # 
 # RCS: @(#) $Id$
 
-set tcltestVersion [package require tcltest]
-namespace import -force tcltest::*
-
-if {$tcl_platform(platform) == "macintosh"} {
-	tcltest::singleProcess 1
-}
-
-tcltest::testsDirectory [file dir [info script]]
+package require Tcl 8.4
+package require tcltest 2.1
+tcltest::configure -testdir [file join [pwd] [file dirname [info script]]]
+tcltest::configure -singleproc 1
+eval tcltest::configure $argv
 tcltest::runAllTests
-
-return
--- tcl/win/buildall.vc.bat
+++ tcl/win/buildall.vc.bat
@@ -12,18 +12,19 @@ echo.
 
 if "%MSVCDir%" == "" call C:\dev\devstudio60\vc98\bin\vcvars32.bat
 set INSTALLDIR=C:\progra~1\tcl
+set TCLDIR=..\..\tcl_head
 
 nmake -nologo -f makefile.vc release winhelp OPTS=none
 if errorlevel 1 goto error
-nmake -nologo -f makefile.vc release OPTS=static
+nmake -nologo -f makefile.vc release OPTS=static,linkexten
 if errorlevel 1 goto error
-nmake -nologo -f makefile.vc core dlls OPTS=static,msvcrt
+nmake -nologo -f makefile.vc core OPTS=static,msvcrt
 if errorlevel 1 goto error
 nmake -nologo -f makefile.vc core OPTS=static,threads
 if errorlevel 1 goto error
-nmake -nologo -f makefile.vc core dlls OPTS=static,msvcrt,threads
+nmake -nologo -f makefile.vc core OPTS=static,msvcrt,threads
 if errorlevel 1 goto error
-nmake -nologo -f makefile.vc shell OPTS=threads
+nmake -nologo -f makefile.vc release OPTS=threads
 if errorlevel 1 goto error
 goto end
 
